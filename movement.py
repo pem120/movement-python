cc7358b8d707fd3c2f8ba0a4680b2aced830ce8b
pem120 <87648658+pem120@users.noreply.github.com>
2021-09-11 19:55:11 +0530

    Add files via upload

---
+++ movement.py
@@ -0,0 +1,32 @@
+hello=[
+["_","o","_"],
+["o","_","_"],
+["_","_","o"],
+]
+i =1
+h=0
+r=1
+while i== 1:
+ if h == -3 :
+    h=0
+ move =input('')
+ if  move == "d" :
+   h -= 1
+   print(hello[h])
+ if  move == "a" :
+   h += 1
+   print(hello[h])
+ if  move == "quit" :
+   i -= 1
+ if  move == "help" :
+   print("   d to move front  a to move back ")
+
+
+
+while r == 1:
+  if move == restart:
+    i=0
+    i=1
+
+
+  
